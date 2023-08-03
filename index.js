7260d898e06394e5eed44bbb8604551695f92128
gavinks07 <71905804+gavinks07@users.noreply.github.com>
2023-08-02 21:27:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Profiles Website</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <!-- Your header content here -->
+    </header>
 
+    <main>
+        <section id="login-section">
+            <!-- User login and registration forms here -->
+        </section>
+
+        <section id="profile-section">
+            <div class="profile-container">
+                <div class="profile-avatar"></div>
+                <div class="profile-username">John Doe</div>
+                <div class="profile-info">Email: john.doe@example.com</div>
+                <div class="profile-info">Age: 30</div>
+                <!-- Add more profile information as needed -->
+            </div>
+        </section>
+
+        <section id="admin-section">
+            <!-- Admin panel section here -->
+        </section>
+    </main>
+
+    <footer>
+        <!-- Your footer content here -->
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
